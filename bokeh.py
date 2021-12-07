0513ffc566f9abc8b878a7ebb901b23786d0ac85
Kolen Cheung <christian.kolen@gmail.com>
2021-12-07 15:30:29 -0800

    update using newer stacks
    now the nbconverted plotly.html also works
    but the pandoc command continues to hang

---
+++ bokeh.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.2
+#   kernelspec:
+#     display_name: all39-defaults
+#     language: python
+#     name: all39-defaults
+# ---
+
+# %%
+import holoviews as hv
+import numpy as np
+
+hv.extension("bokeh")
+
+# %%
+
+# %%
+x = np.linspace(0, np.pi)
+
+# %%
+y = np.sin(x)
+
+# %%
+hv.Curve(np.column_stack((x, y)))
