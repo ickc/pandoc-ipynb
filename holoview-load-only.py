b4e265da481a3788c7d88db8d28584feca2258ba
Kolen Cheung <christian.kolen@gmail.com>
2021-12-09 17:41:26 -0800

    add holoview-load-only.*

---
+++ holoview-load-only.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.3
+#   kernelspec:
+#     display_name: all39-defaults
+#     language: python
+#     name: all39-defaults
+# ---
+
+# %%
+import holoviews as hv
+hv.extension("matplotlib")
